920f05061b1f0b591c100d7568aa89972bb17918
Mahesh Gajadhar <maheshgajadhar1@gmail.com>
2019-03-19 13:11:07 +0100

    Update ios/RNNnlib.m

--- ios/RNNnlib.m
+++ ios/RNNnlib.m
@@ -9,5 +9,21 @@
 }
 RCT_EXPORT_MODULE()
 
+RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
+{
+  RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+}
+RCT_EXPORT_METHOD(addEvent2:(NSString *)name location:(NSString *)location)
+{
+  RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+}
+RCT_EXPORT_METHOD(addEvent3:(NSString *)name location:(NSString *)location)
+{
+  RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+}
+RCT_EXPORT_METHOD(testFunction:(NSString *)name location:(NSString *)location)
+{
+  RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+}
+
 @end
-  
